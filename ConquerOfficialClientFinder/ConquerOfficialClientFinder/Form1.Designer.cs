4d857374c3fc04bfaa746cf23c008fa73fc95a84
DaRkFoxDeveloper <darkfoxdeveloper@gmail.com>
2023-06-03 18:51:17 +0200

    Version v1.1.0.0

--- ConquerOfficialClientFinder/ConquerOfficialClientFinder/Form1.Designer.cs
+++ ConquerOfficialClientFinder/ConquerOfficialClientFinder/Form1.Designer.cs
@@ -48,35 +48,39 @@ namespace ConquerOfficialClientFinder
             // 
             // btnFind
             // 
-            this.btnFind.Location = new System.Drawing.Point(201, 416);
+            this.btnFind.Location = new System.Drawing.Point(268, 520);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnFind.Name = "btnFind";
-            this.btnFind.Size = new System.Drawing.Size(346, 43);
+            this.btnFind.Size = new System.Drawing.Size(461, 54);
             this.btnFind.TabIndex = 0;
             this.btnFind.Values.Text = "Find Clients";
             this.btnFind.Click += new System.EventHandler(this.BtnFind_Click);
             // 
             // lstBoxClients
             // 
-            this.lstBoxClients.Location = new System.Drawing.Point(12, 12);
+            this.lstBoxClients.Location = new System.Drawing.Point(16, 15);
+            this.lstBoxClients.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstBoxClients.Name = "lstBoxClients";
-            this.lstBoxClients.Size = new System.Drawing.Size(879, 345);
+            this.lstBoxClients.Size = new System.Drawing.Size(1172, 431);
             this.lstBoxClients.TabIndex = 1;
             this.lstBoxClients.SelectedIndexChanged += new System.EventHandler(this.LstBoxClients_SelectedIndexChanged);
             // 
             // tbxLimitVersion
             // 
-            this.tbxLimitVersion.Location = new System.Drawing.Point(81, 465);
+            this.tbxLimitVersion.Location = new System.Drawing.Point(108, 581);
+            this.tbxLimitVersion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbxLimitVersion.Name = "tbxLimitVersion";
-            this.tbxLimitVersion.Size = new System.Drawing.Size(103, 32);
+            this.tbxLimitVersion.Size = new System.Drawing.Size(137, 32);
             this.tbxLimitVersion.TabIndex = 2;
             this.tbxLimitVersion.Text = "8000";
             this.tbxLimitVersion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // tbxStartVersion
             // 
-            this.tbxStartVersion.Location = new System.Drawing.Point(81, 427);
+            this.tbxStartVersion.Location = new System.Drawing.Point(108, 534);
+            this.tbxStartVersion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbxStartVersion.Name = "tbxStartVersion";
-            this.tbxStartVersion.Size = new System.Drawing.Size(103, 32);
+            this.tbxStartVersion.Size = new System.Drawing.Size(137, 32);
             this.tbxStartVersion.TabIndex = 3;
             this.tbxStartVersion.Text = "4200";
             this.tbxStartVersion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -87,18 +91,20 @@ namespace ConquerOfficialClientFinder
             // 
             // pBarProgress
             // 
-            this.pBarProgress.Location = new System.Drawing.Point(12, 368);
+            this.pBarProgress.Location = new System.Drawing.Point(16, 460);
+            this.pBarProgress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pBarProgress.Name = "pBarProgress";
-            this.pBarProgress.Size = new System.Drawing.Size(879, 38);
+            this.pBarProgress.Size = new System.Drawing.Size(1172, 48);
             this.pBarProgress.TabIndex = 4;
             this.pBarProgress.Visible = false;
             // 
             // btnDownload
             // 
             this.btnDownload.Enabled = false;
-            this.btnDownload.Location = new System.Drawing.Point(553, 416);
+            this.btnDownload.Location = new System.Drawing.Point(737, 520);
+            this.btnDownload.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDownload.Name = "btnDownload";
-            this.btnDownload.Size = new System.Drawing.Size(338, 92);
+            this.btnDownload.Size = new System.Drawing.Size(451, 115);
             this.btnDownload.TabIndex = 5;
             this.btnDownload.Values.Text = "Download Selected";
             this.btnDownload.Click += new System.EventHandler(this.BtnDownload_Click);
@@ -110,7 +116,8 @@ namespace ConquerOfficialClientFinder
             // 
             // lblFrom
             // 
-            this.lblFrom.Location = new System.Drawing.Point(12, 430);
+            this.lblFrom.Location = new System.Drawing.Point(16, 538);
+            this.lblFrom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lblFrom.Name = "lblFrom";
             this.lblFrom.Size = new System.Drawing.Size(63, 29);
             this.lblFrom.TabIndex = 6;
@@ -118,7 +125,8 @@ namespace ConquerOfficialClientFinder
             // 
             // lblTo
             // 
-            this.lblTo.Location = new System.Drawing.Point(39, 468);
+            this.lblTo.Location = new System.Drawing.Point(52, 585);
+            this.lblTo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lblTo.Name = "lblTo";
             this.lblTo.Size = new System.Drawing.Size(36, 29);
             this.lblTo.TabIndex = 7;
@@ -127,9 +135,10 @@ namespace ConquerOfficialClientFinder
             // pboxAboutImage
             // 
             this.pboxAboutImage.Image = global::ConquerOfficialClientFinder.Properties.Resources.foxlogo;
-            this.pboxAboutImage.Location = new System.Drawing.Point(897, 12);
+            this.pboxAboutImage.Location = new System.Drawing.Point(1196, 15);
+            this.pboxAboutImage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pboxAboutImage.Name = "pboxAboutImage";
-            this.pboxAboutImage.Size = new System.Drawing.Size(340, 345);
+            this.pboxAboutImage.Size = new System.Drawing.Size(453, 431);
             this.pboxAboutImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pboxAboutImage.TabIndex = 8;
             this.pboxAboutImage.TabStop = false;
@@ -137,28 +146,30 @@ namespace ConquerOfficialClientFinder
             // rtboxAbout
             // 
             this.rtboxAbout.InputControlStyle = ComponentFactory.Krypton.Toolkit.InputControlStyle.Ribbon;
-            this.rtboxAbout.Location = new System.Drawing.Point(897, 368);
+            this.rtboxAbout.Location = new System.Drawing.Point(1196, 460);
+            this.rtboxAbout.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rtboxAbout.Name = "rtboxAbout";
             this.rtboxAbout.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.rtboxAbout.Size = new System.Drawing.Size(340, 140);
+            this.rtboxAbout.Size = new System.Drawing.Size(453, 175);
             this.rtboxAbout.TabIndex = 9;
             this.rtboxAbout.Text = "Created with Love for DaRkFoxDeveloper.\nWebsite: www.darkfoxdeveloper.com";
             this.rtboxAbout.WordWrap = false;
             // 
             // btnFindPatches
             // 
-            this.btnFindPatches.Location = new System.Drawing.Point(201, 465);
+            this.btnFindPatches.Location = new System.Drawing.Point(268, 581);
+            this.btnFindPatches.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnFindPatches.Name = "btnFindPatches";
-            this.btnFindPatches.Size = new System.Drawing.Size(346, 43);
+            this.btnFindPatches.Size = new System.Drawing.Size(461, 54);
             this.btnFindPatches.TabIndex = 10;
             this.btnFindPatches.Values.Text = "Find Patches";
             this.btnFindPatches.Click += new System.EventHandler(this.BtnFindPatches_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1249, 516);
+            this.ClientSize = new System.Drawing.Size(1665, 645);
             this.Controls.Add(this.btnFindPatches);
             this.Controls.Add(this.rtboxAbout);
             this.Controls.Add(this.pboxAboutImage);
@@ -171,6 +182,7 @@ namespace ConquerOfficialClientFinder
             this.Controls.Add(this.lstBoxClients);
             this.Controls.Add(this.btnFind);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "ConquerOfficialClientFinder";
             ((System.ComponentModel.ISupportInitialize)(this.pboxAboutImage)).EndInit();
